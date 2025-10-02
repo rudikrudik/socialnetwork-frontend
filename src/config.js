faf94d7f745b451108525bec9ed5f8c6692d70c6
Rudolf Bakhman <rudik.login@gmail.com>
2025-10-02 17:39:56 +0300

    add nginx.conf

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 module.exports = global.config = {
     urls: {
-        baseUrl: "http://192.168.0.199:8000/v1/users",
+        baseUrl: "http://api.vsadmin.ru/v1/users",
+        baseU: "http://192.168.0.202:8001",
         ws: "http://192.168.0.212:8001/ws"
     }
 };
