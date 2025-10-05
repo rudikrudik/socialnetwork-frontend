a7ae9df26201e29aff1e4b7a362833bdcf1e89a2
rudikrudik <rudik.login@gmail.com>
2025-10-05 21:05:48 +0300

    add count to left menu

--- src/Layouts/LeftMenu.jsx
+++ src/Layouts/LeftMenu.jsx
@@ -14,7 +14,22 @@ let size = "12%"
 
 function LeftMenu () {
     let unreadMessages = 0
+    let strUnreadMessages = ""
     const user_id = Cookies.get("user_id");
+
+    const myStyles = {
+        fontSize: "16px",
+        border: "1px solid #9ba3b5",
+        backgroundColor: "#6179a0",
+        margin: "auto",
+        borderRadius: "8px",
+    };
+
+
+    {/*fontSize: "16px",
+    padding: "0px",
+    borderStyle: "solid",
+    borderRadius: "18% 18%",*/}
 
     if (user_id) {
         unreadMessages = CountUnreadMessages(user_id)
@@ -28,7 +43,7 @@ function LeftMenu () {
                 <p><Link to="/friends"><img width={size} alt="Profile Logo" src={friends}/>
                     Friends</Link></p>
                 <p><Link to="/chats"><img width={size} alt="Profile Logo" src={message}/>
-                    Messages { unreadMessages !== 0 ? <p>{unreadMessages}</p> : ""}</Link></p>
+                    Messages { unreadMessages !== 0 ? <span style={myStyles}>&nbsp;+{unreadMessages}&nbsp;</span> : ""}</Link></p>
                 <p><Link to="/news"><img width={size} alt="Profile Logo" src={posts}/>
                     News</Link></p>
                 <p><img width={size} alt="Profile Logo" src={music}/>
